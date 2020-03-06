e958dd76c229935b17abb44e421c95f0c6e46f51
zehoks <56476018+zehoks@users.noreply.github.com>
2020-03-06 23:34:29 +0300

    2 задание было добавлено

--- Белоусов_2072 (1).sql
+++ Белоусов_2072 (1).sql
@@ -83,3 +83,47 @@ FROM hobbies h
 WHERE NOT h.risk >=9999
 ORDER BY h.risk DESC
 LIMIT 3
+--Групповые функции
+--1)Выведите на экран номера групп и количество студентов, обучающихся в них
+SELECT st.n_group, count(*)
+FROM students st
+GROUP BY st.n_group
+ORDER BY st.n_group DESC;
+--2)Выведите на экран для каждой группы максимальный средний балл
+SELECT st.n_group, max(st.score)
+FROM students st
+GROUP BY st.n_group
+ORDER BY st.n_group DESC;
+--3)Подсчитать количество студентов с каждой фамилией
+SELECT COUNT(DISTINCT st.surname)
+FROM students st;
+--5)Для студентов каждого курса подсчитать средний балл
+SELECT substring (st.n_group::varchar, 1, 1) , count (*), st.n_group, st.name, st.surname,avg(st.score)
+FROM students st
+GROUP BY substring (st.n_group::varchar, 1, 1), st.n_group, st.name, st.surname
+ORDER BY st.n_group DESC;
+--6)Для студентов заданного курса вывести один номер групп с максимальным средним баллом
+SELECT substring (st.n_group::varchar, 1, 1) , count (DISTINCT st.n_group), st.n_group, MAX(st.score)
+FROM students st
+GROUP BY substring (st.n_group::varchar, 1, 1), st.n_group
+ORDER BY st.n_group DESC;
+--7)Для каждой группы подсчитать средний балл, вывести на экран только те номера групп и их средний балл, в которых он менее или равен 3.5. Отсортировать по от меньшего среднего балла к большему.
+SELECT st.n_group, st.name, st.surname,avg(st.score)
+FROM students st
+GROUP BY st.n_group, st.name, st.surname
+ORDER BY st.n_group DESC;
+--8)Для каждой группы в одном запросе вывести количество студентов, максимальный балл в группе, средний балл в группе, минимальный балл в группе
+SELECT st.n_group,COUNT(DISTINCT st.surname), max(st.score), avg(st.score), min(st.score)
+FROM students st
+GROUP BY st.n_group
+ORDER BY st.n_group DESC;
+--9)Вывести студента/ов, который/ые имеют наибольший балл в заданной группе
+SELECT st.n_group, st.name, st.surname, st.score
+FROM students st
+WHERE st.score = (SELECT MAX(st.score)FROM students st) AND st.n_group = 2074;
+--10)Аналогично 9 заданию, но вывести в одном запросе для каждой группы студента с максимальным баллом.
+SELECT st.n_group , st.name, st.surname, st.score
+FROM students st
+WHERE st.score = (SELECT MAX(st.score)FROM students st)
+GROUP BY st.n_group
+ORDER BY st.n_group DESC;
